f3a037610c79ccb5423179c0f790ad3fecfd9f2c
florianfmmartin <florian.fm.martin@gmail.com>
2022-04-13 11:37:12 -0400

    feat: debug flag with clap-rs

--- txa/src/main.rs
+++ txa/src/main.rs
@@ -1,17 +1,25 @@
+use clap::Parser;
+
 mod compiler;
 mod interpreter;
 mod stack;
 mod tokenizer;
 
-fn read_file_from_args() -> String {
-    let args: Vec<String> = std::env::args().collect();
-    let filename = &args.get(1).expect("No filename given");
-    std::fs::read_to_string(filename).expect("Could not get content from file")
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Name of the file to run
+    filename: String,
+
+    /// Set debug mode to true
+    #[clap(long)]
+    debug: bool,
 }
 
 fn main() {
-    let debug = false;
-    let file_content = read_file_from_args();
+    let args = Args::parse();
+    let debug = args.debug;
+    let file_content = std::fs::read_to_string(args.filename).expect("Could not get content from file");
     let tokens = tokenizer::tokenize(file_content);
     let declarations = compiler::run(tokens);
     interpreter::run(declarations, debug);
